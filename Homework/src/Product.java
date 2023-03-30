6e6fc73fd28e6f5d01e1bf2e1692f10c4af4ba6b
user <user@DESKTOP-9VU4OK8>
2023-03-30 17:46:28 +0900

    0330

--- Homework/src/Product.java
+++ Homework/src/Product.java
@@ -3,12 +3,13 @@ public class Product {
 
 	String name;
 	int price;
-	
-	Product(){}
-	
-	Product(String name,int price){
+
+	Product() {
+	}
+
+	Product(String name, int price) {
 		this.name = name;
 		this.price = price;
 	}
-	
+
 }
--- Homework/src/hw0330.java
+++ Homework/src/hw0330.java
@@ -5,7 +5,6 @@ public class hw0330 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
 // 문제2) 다음은 키보드로부터 상품 수와 상품 가격을 입력받아서
 // 최고 가격을 가지는 제품과 해당 제품을 제외한 제품들의 총 합을 구하는 프로그램을 작성하세요.
 // 1) 메뉴는 다음과 같이 구성하세요.
@@ -15,57 +14,57 @@ public class hw0330 {
 //	출력예시, "상품명 : 가격"
 // 4) 분석기능은 최고 가격을 가지는 제품과 해당 제품을 제외한 제품들의 총합을 구합니다.
 // 5) 종료 시에는 프로그램을 종료한다고 메세지를 출력하도록 구현하세요.
-				
+
 		Scanner sc = new Scanner(System.in);
 		Product pd[] = null;
 		int pdCount = 0;
-		
-		while(true) {
+
+		while (true) {
 			System.out.println("1. 상품 수 입력 | 2.상품 및 가격입력 | 3. 제품별 가격입력 | 4.분석 | 5.종료");
 			System.out.println("사용할 메뉴 입력 >");
 			int menu = Integer.parseInt(sc.nextLine());
-			
-			if(menu==1) {
+
+			if (menu == 1) {
 				System.out.println("상품 수 입력 >");
 				pdCount = Integer.parseInt(sc.nextLine());
-			}else if(menu==2) {
+			} else if (menu == 2) {
 				pd = new Product[pdCount];
-				for(int i=0; i<pdCount; i++) {
+				for (int i = 0; i < pdCount; i++) {
 					pd[i] = new Product();
-					System.out.println((i+1)+"번째 상품이름 입력");
+					System.out.println((i + 1) + "번째 상품이름 입력");
 					pd[i].name = sc.nextLine();
-					System.out.println((i+1)+"번째 상품가격 입력");
-					pd[i].price=Integer.parseInt(sc.nextLine());
+					System.out.println((i + 1) + "번째 상품가격 입력");
+					pd[i].price = Integer.parseInt(sc.nextLine());
 				}
-			}else if(menu==3) {
-				for(int i =0; i<pdCount;i++) {
-					System.out.println(pd[i].name +":"+ pd[i].price+"원");
+			} else if (menu == 3) {
+				for (int i = 0; i < pdCount; i++) {
+					System.out.println(pd[i].name + ":" + pd[i].price + "원");
 				}
-			}else if(menu==4) {
+			} else if (menu == 4) {
 				int max = pd[0].price;
-				String maxname = ""; 
+				String maxname = "";
 				int sum = 0;
-				for(int i=0;i<pdCount;i++) {
-					if(max<pd[i].price) {
+				for (int i = 0; i < pdCount; i++) {
+					if (max < pd[i].price) {
 						max = pd[i].price;
 						maxname = pd[i].name;
-					}				
-				}
-				for(int i=0;i<pdCount;i++) {
-					if(pd[i].price!=max) {
-					sum += pd[i].price;
 					}
 				}
-				System.out.println("최고가격의 제품과 가격 ("+maxname+":"+max+"원 )");
-				System.out.println("최고가격의 제품을 제외한 제품들의 가격 총합 :" +sum+"원");
-			}else if(menu==5) {
+				for (int i = 0; i < pdCount; i++) {
+					if (pd[i].price != max) {
+						sum += pd[i].price;
+					}
+				}
+				System.out.println("최고가격의 제품과 가격 (" + maxname + ":" + max + "원 )");
+				System.out.println("최고가격의 제품을 제외한 제품들의 가격 총합 :" + sum + "원");
+			} else if (menu == 5) {
 				System.out.println("프로그램을 종료합니다");
 				break;
-			}else {
+			} else {
 				System.out.println("입력이 잘 못 되었습니다.");
 			}
 		}
-		
+
 	}
 
 }
